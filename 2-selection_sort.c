b3a6d435aff4d79703a5f49f8450b709c8761706
kmaangaty <kmaangaty@gmail.com>
2023-06-27 16:26:11 -0700

    2806230226

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,39 @@
 #include "sort.h"
 
 /**
- * selection_sort -  a function that sorts an array of integers in
- * ascending order using the Selection sortalgorithm
+ * selection_sort -  a function that sorts an array of integers
  *
- * @array: input array of integers
- * @size: size of the array
- * Return: no return
+ * @array: parameter of type int
+ * @size: parameter of type struct
+ *
  */
 void selection_sort(int *array, size_t size)
 {
-	int tmp;
-	size_t i, j, small;
+	size_t idx;
+	size_t tdx;
+	size_t sog;
+	int mok;
 
-	for (i = 0; i < size; i++)
+	for (idx = 0; idx < size; idx++)
 	{
-		small = i;
-		for (j = i + 1; j < size; j++)
+		sog = idx;
+
+		for (tdx = idx + 1; tdx < size; tdx++)
 		{
-			if (array[small] > array[j])
-				small = j;
+			if (array[sog] > array[tdx])
+				sog = tdx;
 		}
 
-		if (i != small)
+		if (idx != sog)
 		{
-			tmp = array[i];
-			array[i] = array[small];
-			array[small] = tmp;
+			mok = array[idx];
+
+			array[idx] = array[sog];
+
+			array[sog] = mok;
+
 			print_array(array, size);
+
 		}
 	}
 }
